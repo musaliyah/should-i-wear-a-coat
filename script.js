141bead2ffc3af47882594ce93c6f376b1f9e660
musaliyah <asmusaliar@gmail.com>
2022-10-07 23:02:26 -0700

    attempt at ajax

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ var today = moment().format("MMM Do, YYYY");
 function todaysWeather(city) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-    $.ajax
+    $.ajax({
+        url: apiUrl,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        
+        $("#weatherBars").css("display", "block");
+        $('#cityInfo').empty();
+
+        var icon = response.weather[0].icon;
+        var iconsUrl = `https://openweathermap.org/img/w/${icon}.png`;
+
+    })
 }
